766350c088d7af14e6723cd68af1184ed4cfe3c0
moonjungjo5551 <moonjungjo5551@gmail.com>
2026-06-04 13:10:02 +0900

    클럽하우스 탭 슬라이드 기능 및 화살표 버튼 연동 완료

--- js/index_club.js
+++ js/index_club.js
@@ -1,79 +1,50 @@
 'use strict'
 
 $(document).ready(function() {
+    let current=0;
 
+    $('.club .c_tabs li').click(function(e) {
+         
+        $('.c_area_area > div').removeClass('on');
+        let i = $(this).index();
+        //console.log(i);
+        $('.c_area_area > div').eq(i).addClass('on');
 
-    var myData = [
-        {
-            eng: "Front · Lobby",
-            kor: "프론트 · 로비",
-            intro: "갤러리를 옮겨 놓은 듯 문화와 여유가 숨쉬는 휴게공간",
-            imgUrl: "url(./images/main/robby.png)"
-        },
-        {
-            eng: "Restaurant",
-            kor: "레스토랑",
-            intro: "최고의 셰프들이 선사하는 품격 있는 다이닝 공간",
-            imgUrl: "url(./images/main/restaurant.png)"
-        },
-        {
-            eng: "Banquet Hall",
-            kor: "연회장",
-            intro: "성공적인 비즈니스와 특별한 연회를 위한 고품격 공간",
-            imgUrl: "url(./images/main/hall.png)" 
-        },
-        {
-            eng: "Locker · Spa",
-            kor: "락커 · 스파",
-            intro: "라운드 후 지친 몸과 마음을 풀어주는 편안한 힐링 공간",
-            imgUrl: "url(./images/main/spa.png)" 
-        },
-        {
-            eng: "Pro Shop",
-            kor: "프로샵",
-            intro: "최신 트렌드의 골프 용품과 차별화된 브랜드 전용 매장",
-            imgUrl: "url(./images/main/pro.png)" 
-        }
-    ];
-
-
-    $('.club .c_tabs li a').click(function(e) {
-
-        e.preventDefault();
-
-        var targetLi = $(this).parent('li');
-        var index = targetLi.index(); 
-        
-
-        $('.club .c_tabs li').css('background-color', '');
-        $('.club .c_tabs li').removeClass('on'); 
-        targetLi.addClass('on');           
-        
-
-        targetLi.css('background-color', '#F2E3C2');
-
-
-        var selectedItem = myData[index]; 
-
-        $('.space_name h5').text(selectedItem.eng);
-        $('.space_name h4').text(selectedItem.kor); 
-        $('.space_intro').html(selectedItem.intro); 
-
-
-        $('.space_r').css({
-            'background-image': selectedItem.imgUrl,
-            'background-size': 'cover',
-            'background-position': 'center'
-        });
     });
 
 
-    $('.club .c_tabs li').on('mouseenter', function() {
-        $(this).css('background-color', '#F2E3C2');
-    }).on('mouseleave', function() {
-        if (!$(this).hasClass('on')) {
-            $(this).css('background-color', '#FFF5E2');
-        }
+    $('.club .c_tabs li').click(function() {
+        $('.club .c_tabs li').removeClass('on');
+        $(this).addClass('on');
+
+        let i = $(this).index();
+        movie(i)
+        //console.log("i="+i)
     });
+
+
+    $('.l_btn').click(function(){    
+        current = current - 1 
+        if(current==-1){
+            current=4;
+        }
+        movie(current)
+        console.log("currentL="+current)
+    });
+
+    $('.r_btn').click(function(){
+        current = current + 1;
+        if(current==5){
+            current=0;
+        }
+        movie(current)
+    });
+
+    function movie(i){
+        $('.c_space').removeClass('on');
+        $('.c_space').eq(i).addClass('on');
+
+        current = i;
+    }
 
 });
